93cd469edafc1a327bea99dbaa67571f11342004
gedamote <88816948+gedamote@users.noreply.github.com>
2022-04-07 14:16:14 -0400

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Prints out the last part of a quote in the standard error.
- * Return: 1 if success.
+ * main - print quote
+ *
+ * Description: prints Dors's quote
+ *
+ * Return: Always 1 (Success)
  */
+
 int main(void)
 {
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
